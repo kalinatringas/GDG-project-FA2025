832b294c82459909542ec3d0bb47260e3c475e71
Kalina Tringas <kalinatringas@ufl.edu>
2025-10-22 20:57:55 -0400

    merged routing and navbar together!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home"
 import './App.css'
-import Navbar from './components/Navbar'
+import Profile from "./components/Profile";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div>
+      <Navbar/>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home/>} />
+          <Route path= "/profile" element = {<Profile/>}/>
         </Routes>
+       
       </div>
   )
 }
