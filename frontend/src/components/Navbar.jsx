97c4fb8c98f2179c6dba54f703c36b9fa11c232c
Kalina Tringas <kalinatringas@ufl.edu>
2025-10-24 10:52:15 -0400

    Navbar tweaks

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
 return (
-    <nav className="flex justify-between items-center p-4 bg-gray-800">
+    <nav className="flex justify-between items-center p-4 mt-0 bg-gray-800">
         <h1 className="text-xl font-bold">TBNamed</h1>
         <div className="flex gap-6">
             <a href="/home" className="hover:text-gray-300">Home</a>
