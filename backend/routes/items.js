5a6659411c9da7edd28e7b54ec39c19d2554d846
Kenneth <kokiidoki@gmail.com>
2025-11-02 17:45:55 -0500

    write in placeholders for parts not done yet

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const loggedInTEMP = require('../middleware/TEMP');
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', loggedInTEMP, async (req, res) => {
     const id = req.params.id;
-
-    console.log(id);
+    // get-item logic here
+    
 });
 
 module.exports = router;
