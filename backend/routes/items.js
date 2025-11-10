05b8ae9bbded6a970c0cc58aa569a40b7dff04f6
Kenneth <kokiidoki@gmail.com>
2025-11-10 12:00:50 -0500

    setup placeholder for return values, now just wait for or begin implementing task 2

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -3,10 +3,18 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const loggedInTEMP = require('../middleware/TEMP');
 
-router.get('/:id', loggedInTEMP, async (req, res) => {
-    const id = req.params.id;
-    // get-item logic here
-    
+router.get('/:id', loggedInTEMP, async (req, res, next) => {
+    // TASK 2: 
+    // get item
+    // get user email from item
+    const user_id = undefined;
+    const item_data = {};
+
+
+    // ID field needs to be defined in DB/user.js; current properties are only email and pass
+    // would using email be better to fetch user?
+    const user = await User.findOne({ where: { user_id } });
+    res.status(200).send({item: item_data, email: user.email});
 });
 
 module.exports = router;
