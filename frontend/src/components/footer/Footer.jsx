8db765689940f0e3bc9288612a08e2f10e335ce7
Felix Chang <felixchang@Felixs-MacBook-Air.local>
2025-11-11 12:09:10 -0500

    oops, now it acc works :P

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,8 +1,6 @@
-import { useNavigate } from "react-router-dom";
 import FooterButton from "./FooterButton";
 
 export default function Footer() {
-    const navigate = useNavigate();
     return(
         <div className="flex bg-[#084887]">
             <div className="flex-auto">Bartering App Logo</div>
--- frontend/src/components/footer/FooterButton.jsx
+++ frontend/src/components/footer/FooterButton.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 export default function FooterButton({label, to}) {
+    const navigate = useNavigate();
     return (
         <div className="flex-auto cursor-pointer hover:text-gray-300 hover:underline decoration-solid"
             onClick={() => navigate(to)}>
