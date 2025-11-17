543a2fa199d8fff80de2ce20ea2768744a30a597
Kenneth <kokiidoki@gmail.com>
2025-11-17 16:54:42 -0500

    validate item id in URL

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -4,17 +4,18 @@ const User = require('../models/user');
 const loggedIn = require('../middleware/loggedIn');
 
 router.get('/:id', loggedIn, async (req, res, next) => {
-    // TASK 2: 
-    // get item
-    // get user email from item
-    const user_id = 0;
-    const item_data = {};
+    const url_data = Number(req.params.id);
+    if (Number.isInteger(url_data)) {
+        // TODO: retrieval from DB (task 2)
+        const user_email = "abc@gmail.com";
+        const item_data = {};
 
-
-    // ID field needs to be defined in DB/user.js; current properties are only email and pass
-    // would using email be better to fetch user?
-    const user = await User.findOne({ where: { user_id } });
-    res.status(200).send({item: item_data, email: user.email});
+        // No DB access needed if user email is stored w item data
+        res.status(200).send({item: item_data, email: user_email});
+    } else {
+        // TODO: is this the proper body to send back?
+        res.status(404).send({error: "Item ID not found."});
+    }
 });
 
 module.exports = router;
