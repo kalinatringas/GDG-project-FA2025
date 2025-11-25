cde99637a261030439678b07eb7a64d5b1372f5a
AaronBezi <111883744+AaronBezi@users.noreply.github.com>
2025-11-24 21:20:49 -0500

    Merge branch 'main' into feature/trading-page

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,22 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-<<<<<<< HEAD
-const loggedInTEMP = require('../middleware/TEMP');
-
-router.get('/:id', loggedInTEMP, async (req, res, next) => {
-    // TASK 2: 
-    // get item
-    // get user email from item
-    const user_id = undefined;
-    const item_data = {};
-
-
-    // ID field needs to be defined in DB/user.js; current properties are only email and pass
-    // would using email be better to fetch user?
-    const user = await User.findOne({ where: { user_id } });
-    res.status(200).send({item: item_data, email: user.email});
-=======
 const Item = require('../models/item');
 const loggedIn = require('../middleware/loggedIn');
 
@@ -33,7 +17,6 @@ router.get('/:id', loggedIn, async (req, res, next) => {
         }
     }
     res.status(404).send({error: "Item ID not found.", id: url_data});
->>>>>>> 1c58a6051c7c2a996955869cd35ad70b6db98a62
 });
 
 module.exports = router;
